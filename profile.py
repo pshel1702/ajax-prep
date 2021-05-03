e5b8d087603a5947775a98ee08bf5802bfd93bd5
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-05-03 22:01:55 +0000

    Fixed profile.py

--- profile.py
+++ profile.py
@@ -17,10 +17,12 @@ def profile():
     """Return results from profile form."""
 
     fullname = request.form['name']
-    # TODO: get the values from the rest of the form
-    # Add them to jsonify
+    age = request.form['age']
+    occupation = request.form['occupation']
     
-    return jsonify({'fullname': fullname, })
+    return jsonify({'fullname': fullname, 
+                    'age': age, 
+                    'occupation': occupation})
 
 
 
